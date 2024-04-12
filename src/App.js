5e5bfce46457a527c7e6bb6cbe1a4946f3bb284f
Isabelle Mamprim <mamprimb@ibm.com>
2024-04-12 19:08:29 -0300

    create react/node app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Home } from './Home/Home.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home/Home.js
@@ -0,0 +1,5 @@
+export function Home() {
+    return (
+        <p>oii</p>
+    );
+}
